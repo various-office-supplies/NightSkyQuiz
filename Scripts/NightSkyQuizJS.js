98d89b613cb3bc0014234d949db3729656860457
Benny <85432259+various-office-supplies@users.noreply.github.com>
2026-02-23 18:27:59 -0700

    updated compiler (fixed bug with DS 2025)

--- Scripts/NightSkyQuizJS.js
+++ Scripts/NightSkyQuizJS.js
@@ -1,18 +1,15 @@
-﻿
-// This script is a compiler; it takes all the contents of the
+﻿// This script is a compiler; it takes all the contents of the
 // 		objects file (default 'sample quiz.txt'), analyzes them
-//		line-by-line, then updates the quiz's console accordingly.
+//		line-by-line, then updates the quiz's console.
 
 // I expanded the code here to make it easier to read.
 //		Originally, there was a condensed version in Common that
 //		was smaller, but it wasn't efficient enough to warrant
-//		how terrible it was to read.  So please just use this one :)
+//		how terrible it was to read.  So please enjoy this one :)
 
 
-
-// Customizable stuff (needs changes to Control Panel if >50)
-var max_objects = 50;
-
+// needs changes to Control Panel if >50
+var maxObjects = 50;
 
 
 // These initialize our variables.
@@ -23,7 +20,7 @@ var fileRef,								// Reference for list file.
 	stickFade        = false, 				// true = constellation sticks will fade out after use
 	alphabetizeList  = false,				// true = Objects are sorted by Name, false = Objects are stored by type (constellation, direction, etc.)
 	categorizeList   = false,				// true = follow constellations with their stars
-	listenForComv    = false,				// is true if current line is input for '!addCommands'
+	listenForCmd     = false,				// is true if current line is input for '!addCommands'
 	listenForDisplay = false,				// is true if current line is input for '!info'
 	listenForOrder   = false,				// is true if current line is input for '!order'
 
@@ -42,8 +39,6 @@ try
 { 
 	fileRef = Ds.FileOpen(Ds.FileGetOpenName(), 'r');
 }
-
-	//If file select is canceled by user, saves in messages and disables script.
 	catch(err)
 	{
 		print('No file selected!');
@@ -62,25 +57,22 @@ try
 // loop until End-of-File
 while( Ds.FileEOF(fileRef) == 0 )
 {
-
-	// Defines lineContents as the text on current line
-	var lineContents = Ds.FileReadLine(fileRef).replace(/[^\x00-\x7F]/g,'');
+	// text on current line
+	var lineContents = Ds.FileReadLine(fileRef).replace(/[^\x00-\x7F]/g,'').trim();
 	
-	//		then if first character of line is empty, or includes "#", or only includes spaces: skip
-		if( !lineContents[0] || lineContents[0]=='\#'|| (lineContents[0]=='\t' && (lineContents.length==1 || lineContents[1]==' ' || lineContents[1]=='\t')) ) continue;
+	// skip if: line is blank, OR starts with "#"
+	if( !lineContents[0]  ||  lineContents[0] == '\#' )
+		continue;
 	
-	// 		then increases currentLine by 1.
-		lineContents = lineContents[0] + lineContents.slice( (1-lineContents.length) );
-		currentLine++;
-	
+	currentLine++;
 	
 	// If 2nd character of current line is a '-', it recognizes
 	// 		line as a category and updates relavent variables.
 	if ( lineContents[1] == '-' )
 	{
-		listenForComv    = false;
 		listenForDisplay = false;
 		listenForOrder   = false;
+		listenForCmd     = false;
 		
 		var category = lineContents[3].toLowerCase();
 		last = category;
@@ -92,9 +84,9 @@ while( Ds.FileEOF(fileRef) == 0 )
 		// 		searches for known function. If not recognized, prints error.
 		else if( lineContents[0] == '\!' )
 		{
-			listenForComv    = false;
 			listenForDisplay = false;
 			listenForOrder   = false;
+			listenForCmd     = false;
 			
 			if( lineContents.slice(1,5) == 'date' )
 			{
@@ -105,72 +97,56 @@ while( Ds.FileEOF(fileRef) == 0 )
 				else if( lineContents.slice(1,6) == 'stick' ) stickToggle = true;
 				else if( lineContents.slice(1,6) == 'order' ) listenForOrder = true;
 				else if( lineContents.slice(1,12) == 'alphabetize' ) alphabetizeList = true;
-				else if( lineContents.slice(1,11) == 'addCommand' ) listenForComv = true;
+				else if( lineContents.slice(1,11) == 'addCommand' ) listenForCmd = true;
 				else if( lineContents.slice(1,5) == 'info' ) listenForDisplay = true;
 				else print('Check your spelling! [line '+currentLine+']');
 		}
 		
 		// If listening for !addCommands, sends current line as Digistar command.
-		else if ( listenForComv ) Ds.SendStringCommand( lineContents.slice(1, lineContents.length) );
+		else if ( listenForCmd )
+			Ds.SendStringCommand( lineContents );
 		
 		// If listening for !info, saves current line as string in infoList.
-		else if ( listenForDisplay ) infoList.push(lineContents.slice(1,lineContents.length));
+		else if ( listenForDisplay ) 
+			infoList.push( lineContents );
 		
 		// If listening for !order, saves current line as string in orderList.
-		else if ( listenForOrder ) orderList.push(lineContents.slice(1,lineContents.length)); 
+		else if ( listenForOrder ) 
+			orderList.push( lineContents ); 
 		
 		// If an object to add, adds current line to objList.
-		else if ( true )
+		else
 		{
-			var obj_name = lineContents.slice(1, lineContents.length).trim();
+			var objName = lineContents;
 			
-			objList.push( obj_name );				// save object
-			category__obj[ obj_name ] = last;		// save keypair object-category so we can lookup category later
+			objList.push( objName );				// save object
+			category__obj[ objName ] = last;		// save keypair object-category so we can lookup category later
 			objUsedSoFar++;
-			
-		// If line does not meet any of above criteria, it assumes end-of-file was hit and stops loop.
-		} else
-		{
-			break;
-			/*
+		} 
 
-	-->	This Windows API call doesn't isn't sent correctly by digistar. Ask digistar support about PopupMessage() starting minimized to taskbar.	
+		/*
+
+	--> This Windows API call is sent incorrectly by digistar. 
+	--> I eventually need to ask digistar support about PopupMessage() starting minimized to taskbar.	
 	
-			var stopQuiz=Ds.PopupMessage('Reached max of '+max_objects+' items at line '+currentLine+' in list.\n\n--> Press OK to use current items only.\n--> Press CANCEL if necessary items might be excluded.','Continue\?','OKCANCEL','ICONWARNING','TOPMOST');
-			if (stopQuiz==1) break;
-			else if (stopQuiz==2) {
-				objCategories=[];
+			var stopQuiz = Ds.PopupMessage('Reached max of ' + maxObjects + ' items at line ' + currentLine + ' in list.\n\n--> Press OK to use current items only.\n--> Press CANCEL if necessary items might be excluded.','Continue\?','OKCANCEL','ICONWARNING','TOPMOST');
+			if ( stopQuiz == 1 ) break;
+			else if ( stopQuiz == 2 )
+			{
+				objCategories = [];
 				break;
 			}
 			
-			*/
-		}
+		*/
 }
 
-
-// Done reading file; continuing rest of script.
-Ds.FileClose(fileRef);
+Ds.FileClose( fileRef );
 
 
-// if error, report to console what happened
 } catch(err)
 {
-	throw ('Manual Error Report:\n-> Crashed at line ['+currentLine+'] of txt file;\n-> # of objects imported: '+objUsedSoFar+';\n'+err);
+	throw ('Manual Error Report:\n-> Crashed at line [' + currentLine + '] of txt file;\n-> # of objects imported: ' + objUsedSoFar + ';\n' + err);
 }
-
-/*
-for (let cQ=0;cQ<infoList.length;cQ++){
-		var currentInfoItem='fancyMessage'+(cQ>8?(cQ+1):'0'+(cQ+1));
-	Ds.CreateObject(currentInfoItem,'textClass','temporary');
-		Ds.SetObjectAttr(currentInfoItem,'text',infoList[cQ]);
-		Ds.SetObjectAttr(currentInfoItem,'position',{x:0,y:10*cQ,z:2,unit:'m',mode:'SPHERICAL'});
-		Ds.SetObjectAttr(currentInfoItem,'textScale',{x:1,y:1});
-		Ds.SetObjectAttr(currentInfoItem,'color',{r:100,g:100,b:100});
-		Ds.SetObjectAttr(currentInfoItem,'intensity',100);
-	Ds.SceneAddObject(null,currentInfoItem,'fixed');
-}
-*/
-
 
 
 // Changes order of objects (alphabetize + !order)
@@ -178,9 +154,9 @@ if( orderList.length )	// true if we saw !order command
 {
 	var objListCopy = [];
 	
-	for (var obj_index = 0;  obj_index < orderList.length;  obj_index++)
+	for (var i=0;  i < orderList.length;  i++)
 	{
-		var obj = orderList[obj_index];
+		var obj = orderList[i];
 		
 		if( objList.contains( obj ) )
 		{
@@ -189,9 +165,9 @@ if( orderList.length )	// true if we saw !order command
 		}
 	}
 	
-	for (var obj_index = 0;  obj_index < objList.length;  obj_index++)
+	for (var i=0;  i < objList.length;  i++)
 	{
-		var obj = objList[obj_index];
+		var obj = objList[i];
 		
 		if( !objListCopy.contains( obj ) )
 		{
@@ -200,8 +176,8 @@ if( orderList.length )	// true if we saw !order command
 		}
 	}
 	
-//	print(orderList);
-//	print(objList);
+//	print( orderList );
+//	print( objList );
 //	print( objListCopy );
 //	print('and, alpheratz, m31, aql, saturn, ...');
 		
@@ -215,49 +191,52 @@ if( orderList.length )	// true if we saw !order command
 
 } else	// didn't see !order or !alphabetize
 {
-	objList = objList;
+//	objList = objList;
 	
 }
 
 
+var colors = {
+	'd': [255, 220, 210],		// directions (d) + default
+	'c': [255, 246, 204],		// constellations (c)
+	's': [240, 172, 117],		// stars (s)
+	'm': [232, 112, 95],		// messier
+	'o': [247, 114, 105],		// objects (o)
+	'p': [63, 118, 196]			// planets (p)
+}
+var percent = 100/255;
 
-// This part adds the buttons and applies their properties.
-var btn_ID = 0;
-for (var obj_index = 0;  obj_index < objList.length;  obj_index++)
+function giveMeColor(category)
 {
-	var obj = objList[obj_index];
-	btn_ID++;
-	
-	if( btn_ID > max_objects )break;	// quit adding buttons if reached max
-	
-	var category = category__obj[ obj ];
-	var buttonName = 'quizButton' +  (btn_ID < 10  ?'0'  :'') + btn_ID;
-	Ds.CreateObject( buttonName, 'buttonClass' );
-	
-	var color_shift = 100/255;
-	var color;
-	var obj_colors = {
-		'd': [255, 220, 210],		// directions (d) + default
-		'c': [255, 246, 204],		// constellations (c)
-		's': [240, 172, 117],		// stars (s)
-		'm': [232, 112, 95],		// messier
-		'o': [247, 114, 105],		// objects (o)
-		'p': [63, 118, 196]			// planets (p)
-	}
-	
-	if ( obj_colors[category] )
-		color = {
-			'r': color_shift * obj_colors[category][0],
-			'g': color_shift * obj_colors[category][1],
-			'b': color_shift * obj_colors[category][2]
+	if ( colors[category] )
+		return {
+			'r': percent * colors[category][0],
+			'g': percent * colors[category][1],
+			'b': percent * colors[category][2]
 		};
 	else
-		color = {
-			'r': color_shift * obj_colors.d[0],
-			'g': color_shift * obj_colors.d[1],
-			'b': color_shift * obj_colors.d[2]
+		return {
+			'r': percent * colors.d[0],
+			'g': percent * colors.d[1],
+			'b': percent * colors.d[2]
 		}
+}
+
+
+// This part adds the buttons and applies their properties.
+var button = 0;
+for (var i=0;  i < objList.length;  i++)
+{
+	var obj = objList[i];
+	button++;
 	
+	if( button > maxObjects )break;	// quit adding buttons if reached max
+	
+	var category = category__obj[ obj ];
+	var buttonName = 'quizButton' +  (button < 10  ?'0'  :'') + button;
+	Ds.CreateObject( buttonName, 'buttonClass' );
+	
+	var color = giveMeColor(category);
 	
 	Ds.SetObjectAttr( buttonName,'label', obj );
 	Ds.SetObjectAttr( buttonName,'fontsize', 32 );
@@ -265,11 +244,10 @@ for (var obj_index = 0;  obj_index < objList.length;  obj_index++)
 	Ds.SetObjectAttr( buttonName,'labelColor', { r:0, g:0, b:0 } );
 	Ds.SetObjectAttr( buttonName,'borderColor', color );
 	
-//	Ds.SetObjectAttr( buttonName, 'command', "js play Common/Night Sky Quiz JS/API_sandwich.js|starnumber('"+obj+"','"+category+"','"+buttonName+"',"+stickToggle+","+stickFade+",true)");
-	Ds.SetObjectAttr( buttonName, 'command', "js play ./API.js|starnumber('"+obj+"','"+category+"','"+buttonName+"',"+stickToggle+","+stickFade+",true)");
+	Ds.SetObjectAttr( buttonName, 'command', "js play ./API.js|selectObject('" + obj + "','" + category + "','" + buttonName + "'," + stickToggle + "," + stickFade + ",true)");
 	
 }
 
 
 
-}	// goodbye!
+}	// Goodbye!
